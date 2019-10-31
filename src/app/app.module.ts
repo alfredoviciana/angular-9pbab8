4ed8d9c41ace2cb2fa888171b7a833cc16e41103
alfredoviciana <windcash@gmail.com>
2019-10-31 23:55:42 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, 
+  LeafletModule.forRoot()
+   ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
